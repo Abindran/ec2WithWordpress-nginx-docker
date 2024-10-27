a1669ff3ee5b1638e00b5196be9c700ee02f6adf
abindranr <74658327+Abindran@users.noreply.github.com>
2024-10-27 16:21:47 +0530

    Update 02-init-infra.sh

--- 02-init-infra.sh
+++ 02-init-infra.sh
@@ -14,7 +14,7 @@ EOF
 
 echo "Creating a Docker app network"
 docker network create app-network
-docker compose -f docker-infra-compose.yml up -d 
+docker-compose -f docker-infra-compose.yml up -d 
 
 echo "Infra setup complete. Please wait 2 minutes for the services to be up and running"
 
@@ -22,7 +22,7 @@ echo "Infra setup complete. Please wait 2 minutes for the services to be up and 
 seconds=0
 max_wait=120
 while [ $seconds -lt $max_wait ]; do
-    if docker compose -f docker-infra-compose.yml ps | grep -q "starting\|unhealthy"; then
+    if docker-compose -f docker-infra-compose.yml ps | grep -q "starting\|unhealthy"; then
         echo -ne "Waiting for services to start: [$(printf "%0.s=" $(seq 1 $((seconds/2))))$(printf "%0.s " $(seq $(((120-seconds)/2)) 1 59))]\r"
         sleep 1
         ((seconds++))
@@ -33,6 +33,6 @@ while [ $seconds -lt $max_wait ]; do
     fi
 done
 
-if [$seconds -eq $max_wait]; then
+if [ $seconds -eq $max_wait ]; then
     echo -e "\nReached the maximum wait time of 2 minutes."
 fi
